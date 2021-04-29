c1cfca2f66dd8d5fadf734aa767fd9f1fbf8a8e3
tdavis-credera <tim.davis@credera.com>
2021-04-29 11:12:38 -0500

    updates

--- initialization/setup.sh
+++ initialization/setup.sh
@@ -73,9 +73,13 @@ kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
 
 #install metalLB
 https://metallb.universe.tf/installation/
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 # Install nginx
-helm install nginx-ingress
+helm install ingress-nginx nginx-stable/nginx-ingress
 
 #nfs common tools
 sudo apt install nfs-common
